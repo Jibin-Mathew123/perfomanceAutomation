09fd74dd94dc2566192ab1f67827af270369dfac
Jibin Mathew <54473571+Jibin-Mathew123@users.noreply.github.com>
2025-04-14 21:16:21 +0530

    ading changes

---
+++ src/main/java/utils/AuthTokenGenerator.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class AuthTokenGenerator {
+    public static String generateToken() {
+        // Replace with your actual logic to generate token
+        return "Bearer " + System.currentTimeMillis();
+    }
+}
